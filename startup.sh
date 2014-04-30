aaac00b449d076b9096c92a83237f8b6e21dc8cd
Justin Ball <justinball@gmail.com>
2014-04-29 22:46:09 -0600

    Don't use path for sed

--- startup.sh
+++ startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/usr/bin/sed -i "s/<proxy_pass_addr>/${ELASTICSEARCH_PORT}/" /etc/nginx/nginx.conf
+sed -i "s/<proxy_pass_addr>/${ELASTICSEARCH_PORT}/" /etc/nginx/nginx.conf
 service nginx start
