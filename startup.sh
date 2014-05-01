fe690fdfd4d0eb358d181405cca7173efac73980
Justin Ball <justinball@gmail.com>
2014-04-30 22:41:22 -0600

    Use the correct var names

--- startup.sh
+++ startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sed -i "s|<proxy_pass_addr>|$DB_PORT_9200_TCP_ADDR:$DB_PORT_9200_TCP_PORT|" /etc/nginx/nginx.conf
+sed -i "s|<proxy_pass_ip>|$ELASTICSEARCH_PORT_9200_TCP_ADDR|" /etc/nginx/nginx.conf
+sed -i "s|<proxy_pass_port>|$ELASTICSEARCH_PORT_9200_TCP_PORT|" /etc/nginx/nginx.conf
 nginx
