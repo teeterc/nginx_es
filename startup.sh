de9fb16c39b2f8b2a7ca5e45b08bd2f0d79860f0
Justin Ball <justinball@gmail.com>
2014-05-28 22:45:03 -0600

    fixed startup script

--- startup.sh
+++ startup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i "s|<proxy_pass_ip>|$ELASTICSEARCH_PORT_9200_TCP_ADDR|" /etc/nginx/nginx.conf
-sed -i "s|<proxy_pass_port>|$ELASTICSEARCH_PORT_9200_TCP_PORT|" /etc/nginx/nginx.conf
+sed -i "s|<proxy_pass_ip>|$ELASTICSEARCH_PORT_9200_TCP_ADDR|" /etc/nginx/nginx_es.conf
+sed -i "s|<proxy_pass_port>|$ELASTICSEARCH_PORT_9200_TCP_PORT|" /etc/nginx/nginx_es.conf
 
 nginx
